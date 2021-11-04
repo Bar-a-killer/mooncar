4e035543e0b063df9f37b94597412b084f949a39
Bar <90964826+Bar-a-killer@users.noreply.github.com>
2021-11-04 17:24:31 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,30 +1,3 @@
-function 循線 () {
-    while (mooncar.IRRead() != 無感光) {
-        if (mooncar.IRRead() == 全感光) {
-            mooncar.MoonCarGo(mooncar.Direction.direct1, 20)
-        } else if (mooncar.IRRead() == 左感光) {
-            mooncar.MoonCarGo(mooncar.Direction.direct4, 20)
-        } else if (mooncar.IRRead() == 右感光) {
-            mooncar.MoonCarGo(mooncar.Direction.direct3, 20)
-        }
-    }
-}
-let 全感光 = 0
-let 左感光 = 0
-let 右感光 = 0
-let 無感光 = 0
-無感光 = 3
-右感光 = 2
-左感光 = 1
-全感光 = 0
-let supersound = 1
-basic.forever(function () {
-    if (supersound % 2 == 1) {
-        循線()
-    } else {
-        basic.pause(6000)
-    }
-})
 /**
  * def 超音波觸發():
  * 
@@ -56,6 +29,34 @@ basic.forever(function () {
  * 
  * basic.pause(500)
  */
+function 循線 () {
+    while (mooncar.IRRead() != 無感光) {
+        if (mooncar.IRRead() == 全感光) {
+            mooncar.MoonCarGo(mooncar.Direction.direct1, 20)
+        } else if (mooncar.IRRead() == 左感光) {
+            mooncar.MoonCarGo(mooncar.Direction.direct4, 20)
+        } else if (mooncar.IRRead() == 右感光) {
+            mooncar.MoonCarGo(mooncar.Direction.direct3, 20)
+        }
+    }
+}
+let 全感光 = 0
+let 左感光 = 0
+let 右感光 = 0
+let 無感光 = 0
+mooncar.Filllight(mooncar.Switch.Open)
+無感光 = 3
+右感光 = 2
+左感光 = 1
+全感光 = 0
+let supersound = 1
 basic.forever(function () {
-    mooncar.Filllight(mooncar.Switch.Open)
+    if (supersound % 2 == 1) {
+        循線()
+    } else {
+        basic.pause(6000)
+    }
+})
+basic.forever(function () {
+	
 })
