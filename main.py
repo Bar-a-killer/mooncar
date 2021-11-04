312064f3c7fcc2f063871fc07b955c5526ecaecd
Bar <90964826+Bar-a-killer@users.noreply.github.com>
2021-11-04 12:30:16 +0800

    更新 pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,50 @@
+def 循線():
+    while mooncar.ir_read() != 無感光:
+        if supersound == 1:
+            break
+        elif mooncar.ir_read() == 全感光:
+            while mooncar.ir_read() == 全感光:
+                mooncar.moon_car_go(mooncar.Direction.DIRECT1, 20)
+        elif mooncar.ir_read() == 左感光:
+            while mooncar.ir_read() != 全感光:
+                mooncar.moon_car_go(mooncar.Direction.DIRECT4, 20)
+        elif mooncar.ir_read() == 右感光:
+            while mooncar.ir_read() != 全感光:
+                mooncar.moon_car_go(mooncar.Direction.DIRECT3, 20)
+def 超音波觸發():
+    mooncar.moon_car_go(mooncar.Direction.DIRECT4, 15)
+    basic.pause(500)
+    mooncar.moon_car_lr(20, 90)
+    basic.pause(4000)
+    mooncar.moon_car_go(mooncar.Direction.DIRECT4, 15)
+    basic.pause(500)
+_1 = 0
+supersound = 0
+全感光 = 0
+左感光 = 0
+右感光 = 0
+無感光 = 0
+無感光 = 3
+右感光 = 2
+左感光 = 1
+全感光 = 0
+ 
 def on_forever():
-    pass
+    global _1
+    while supersound == 1:
+        basic.show_number(_1)
+        basic.pause(1000)
+        _1 += 1
 basic.forever(on_forever)
+ 
+def on_forever2():
+    global supersound
+    if mooncar.ultrasonic_sensor() <= 10:
+        supersound = 1
+        超音波觸發()
+        supersound = 0
+    else:
+        循線()
+basic.forever(on_forever2)
+ 
+
--- main.ts
+++ main.ts
@@ -1,3 +1,53 @@
+function 循線 () {
+    while (mooncar.IRRead() != 無感光) {
+        if (supersound == 1) {
+            break;
+        } else if (mooncar.IRRead() == 全感光) {
+            while (mooncar.IRRead() == 全感光) {
+                mooncar.MoonCarGo(mooncar.Direction.direct1, 20)
+            }
+        } else if (mooncar.IRRead() == 左感光) {
+            while (mooncar.IRRead() != 全感光) {
+                mooncar.MoonCarGo(mooncar.Direction.direct4, 20)
+            }
+        } else if (mooncar.IRRead() == 右感光) {
+            while (mooncar.IRRead() != 全感光) {
+                mooncar.MoonCarGo(mooncar.Direction.direct3, 20)
+            }
+        }
+    }
+}
+function 超音波觸發 () {
+    mooncar.MoonCarGo(mooncar.Direction.direct4, 15)
+    basic.pause(500)
+    mooncar.MoonCarLR(20, 90)
+    basic.pause(4000)
+    mooncar.MoonCarGo(mooncar.Direction.direct4, 15)
+    basic.pause(500)
+}
+let _1 = 0
+let supersound = 0
+let 全感光 = 0
+let 左感光 = 0
+let 右感光 = 0
+let 無感光 = 0
+無感光 = 3
+右感光 = 2
+左感光 = 1
+全感光 = 0
 basic.forever(function () {
-    music.playMelody("- - - - - - - - ", 120)
+    while (supersound == 1) {
+        basic.showNumber(_1)
+        basic.pause(1000)
+        _1 += 1
+    }
+})
+basic.forever(function () {
+    if (mooncar.UltrasonicSensor() <= 10) {
+        supersound = 1
+        超音波觸發()
+        supersound = 0
+    } else {
+        循線()
+    }
 })
